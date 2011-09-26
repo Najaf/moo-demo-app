2bbc16788e1647452adb0934e696c8f32b8df474
Najaf Ali <ali.najaf@gmail.com>
2011-09-26 07:22:11 -0400

    made twitter minicards form a bit prettier

--- Gemfile
+++ Gemfile
@@ -1,8 +1,5 @@
 source "http://rubygems.org"
 gem 'sinatra'
-gem 'oauth'
 gem 'json'
-gem 'sqlite3'
-gem 'nokogiri'
 gem 'twitter'
 gem 'moo', git: 'git://github.com/Najaf/moo.rb.git'
--- moohack.rb
+++ moohack.rb
@@ -1,108 +1,25 @@
 # encoding: UTF-8
 require 'sinatra'
-require 'oauth'
-require 'erb'
 require 'json'
-require 'sqlite3'
-require 'nokogiri'
-require 'moo'
 require 'twitter'
 require 'yaml'
-
+require 'moo'
 
 enable :sessions
 
-class Client
-
-  def self.oauth_secret=(secret)
-    @@oauth_secret = secret
-  end
-
-  def self.oauth_key=(key)
-    @@oauth_key = key
-  end
-
-  def self.config
-    yield self if block_given?
-  end
-
-
-  attr_accessor :oauth_consumer, :oauth_key, :oauth_secret
-  def initialize(params = nil)
-    unless params.nil?
-      @oauth_secret = params[:oauth_secret]
-      @oauth_key    = params[:oauth_key]
-    end
-
-    yield self if block_given?
-
-    ## set key/secret to default if not set
-    @oauth_secret ||= @@oauth_secret
-    @oauth_key    ||= @@oauth_key
-
-    ## error if the oauth secret and key have not been set by now
-    unless @oauth_secret && @oauth_key  
-      raise "Moo Client requires OAuth key/secret to be set"
-    end
-
-    initialize_oauth_consumer
-  end
-
-  # get the initial request token
-  #
-  # clients can authorize this token and get the subsequent access token
-  def get_request_token(callback_or_hash)
-    callback = callback_or_hash.is_a?(String) ? callback_or_hash : callback_or_hash[:callback]
-    @oauth_consumer.get_request_token(
-      oauth_callback: callback
-    )
-  end
-
-  def create_pack(access_token, product_code, pack_json)
-    call_api(access_token, {
-      method:   'moo.pack.createPack',
-      product:  product_code.to_s,
-      pack:     pack_json
-    })
-  end
-
-  private
-    def initialize_oauth_consumer
-      @oauth_consumer = OAuth::Consumer.new(
-        @oauth_key,
-        @oauth_secret,
-        {
-          site:               "https://secure.moo.com",
-          request_token_path: "/oauth/request_token.php",
-          access_token_path:  "/oauth/access_token.php",
-          authorize_path:     "/oauth/authorize.php" 
-        }
-      )
-    end
-
-    def call_api(access_token, params = {})
-      access_token.post(
-        '/api/service/',
-        params,
-        {
-          'Content-Type' => 'application/json'
-        }
-      )
-    end
-end
-
 app_config = YAML::load_file('config.yml')['moo']
 
-Client.config do |c|
+Moo::Client.config do |c|
   c.oauth_key = app_config['key']
   c.oauth_secret = app_config['secret']
 end
 
 
-
 # puts request token in the session
+# this is very dumb. It means that your oauth tokens are stored in cookies.
+# Don't do this in a production app, really.
 def start_auth
-  @client = Client.new
+  @client = Moo::Client.new
   @request_token = @client.get_request_token callback: 'http://localhost:9393/input'
 
   session[:request_token] = @request_token
@@ -155,14 +72,7 @@ post '/pack' do
         end
       end
 
-      # need to do something automated about side numbering...
-      # maybe just stuff this before the 'to_json' call?
-      counter = 1
-      p.sides.each do |s|
-        s.side_num = counter
-        counter += 1
-      end
-
+      p.fill_side_nums
     end
     #json_obj = JSON.load(pack)
     #fresh_json = JSON.generate(json_obj)
@@ -170,7 +80,7 @@ post '/pack' do
     # try sending it to moo
     access_token
     at = session[:access_token]
-    client = Client.new
+    client = Moo::Client.new
     @res = client.create_pack(at, 'minicard', @pack.to_json)
     response = JSON.load(@res.body)
 
